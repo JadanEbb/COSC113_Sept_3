84a57293e3906de6a9bd19d89bea02219df1056b
thoubot <ebbj0908@students.bowiestate.edu>
2026-09-03 10:22:36 -0400

    OC - 4 (CC)

--- src/Student.java
+++ src/Student.java
@@ -22,11 +22,11 @@ public class Student {
         Student.LN = LN;
     }
 
-
     //How many parameters? () - 3
     Student(String FN, String LN, Integer SID) {
-        Student.FN = FN;
-        Student.LN = LN;
+//        Student.FN = FN;
+//        Student.LN = LN;
+        this(FN,LN);
         this.SID = SID;
     }
 
@@ -34,14 +34,15 @@ public class Student {
         Student.FN = FN;
         Student.LN = LN;
         this.SID = SID;
+
     }
 
     // You can create overloaded constuctors by varying 1) The parameter number, 2 order
-    public static void main(String[] args) {
+    static void main(String[] args) {
 
         Student s4 = new Student( "Joe", "Dirt");
-        System.out.println(s4.FN);
-        System.out.println(s4.LN);
+        System.out.println(FN);
+        System.out.println(LN);
         System.out.println(s4.SID);
         System.out.println("------------------------");
 
@@ -50,20 +51,24 @@ public class Student {
         Student s1;
         s1 = new Student("FN", "LN", 1);
         System.out.println(s1);
-        System.out.println(s1.FN);
+        System.out.println(FN);
 
     Student s2;
     s2 = new Student("Tobiloba", "Ayodeji", 14141);
-    System.out.println(s2.FN);
-    System.out.println(s2.LN);
+    System.out.println(FN);
+    System.out.println(LN);
 
     Student s3;
     s3 = new Student("Miguel", "Gascaortaga", 31415);
-    System.out.println(s3.FN);
-    System.out.println(s3.LN);
+    System.out.println(FN);
+    System.out.println(LN);
 
     Student s5;
     s5 = new Student(FN, Sid, LN);
+    System.out.println(FN);
+    System.out.println(LN);
+
+
 
         //Student success center 180? tue thur 3-5pm
 // Argument is the value
